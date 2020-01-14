232432f0bcb246c89b1833af8cd617ede594c09e
Marcus Spiegel <spiegel@uscreen.de>
2020-01-14 13:56:08 +0100

    another test

--- test/cli-list.test.js
+++ test/cli-list.test.js
@@ -1,12 +1,23 @@
 const tap = require('tap')
 const { cli } = require('./helper')
 
-tap.test('$ cli list', async t => {
+tap.test('$ cli list (on empty directory)', async t => {
   const result = await cli(['list'], './test/_fixtures/empty')
   t.strictEqual(0, result.code, 'Should succeed')
   t.strictEqual(
     true,
     result.stdout.startsWith('demorepo - missing: no directory'),
+    'Should print error information on missing directory'
+  )
+  t.end()
+})
+
+tap.test('$ cli list (with missing checkout)', async t => {
+  const result = await cli(['list'], './test/_fixtures/norepos')
+  t.strictEqual(0, result.code, 'Should succeed')
+  t.strictEqual(
+    true,
+    result.stdout.startsWith('demorepo - missing: no repository'),
     'Should print error information on empty repos'
   )
   t.end()
