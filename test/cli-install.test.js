b037175c332cd22182be8702fd36a9ffb3489455
Marcus Spiegel <spiegel@uscreen.de>
2020-01-15 17:09:17 +0100

    snapshot stubbed demorepo

--- test/cli-install.test.js
+++ test/cli-install.test.js
@@ -1,14 +1,14 @@
 const tap = require('tap')
 const { cli } = require('./helper')
 
-// tap.test('$ cli list (on empty directory)', async t => {
-//   const result = await cli(['install'], './test/_fixtures/addrepos')
-//   t.strictEqual(0, result.code, 'Should succeed')
-//   console.log(result.stdout)
-//   // t.strictEqual(
-//   //   true,
-//   //   result.stdout.startsWith('demorepo - missing: no directory'),
-//   //   'Should print error information on missing directory'
-//   // )
-//   t.end()
-// })
+tap.test('$ cli list (on empty directory)', async t => {
+  const result = await cli(['install'], './test/_fixtures/addrepos')
+  t.strictEqual(0, result.code, 'Should succeed')
+  console.log(result.stdout)
+  // t.strictEqual(
+  //   true,
+  //   result.stdout.startsWith('demorepo - missing: no directory'),
+  //   'Should print error information on missing directory'
+  // )
+  t.end()
+})
